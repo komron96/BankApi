1c3fcf8a346b881410a0f57b0d9c25e458ff65b1
komron96 <komron.saydaliev@gmail.com>
2024-07-01 18:10:34 +0500

    User configuraion

--- BankApp.API/Controllers/ClientController.cs
+++ BankApp.API/Controllers/ClientController.cs
@@ -1,55 +1,70 @@
 using DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
 
-namespace BankApp.WebApi;
-
-
-[ApiController]
-[Route("user")]
-public sealed class ClientContoller : ControllerBase
+namespace BankApp.WebApi
 {
-    private readonly AppDbContext _appDbContext;
 
-    public ClientContoller(AppDbContext appDbContext)
+    [ApiController]
+    [Route("client")]
+    public sealed class ClientContoller : ControllerBase
     {
-        _appDbContext = appDbContext;
-    }
+        private readonly AppDbContext _appDbContext;
 
-    [HttpPost]
-    public async Task<Client> CreateClientAsync([FromBody] Client client, CancellationToken cancellationToken)
-    {
-        await _appDbContext.AddAsync(client, cancellationToken);
-        await _appDbContext.SaveChangesAsync(cancellationToken);
-        return client;
-    }
-
-
-    [HttpGet]
-    public async Task<IEnumerable<Client>> GetClientsAsync(CancellationToken cancellationToken)
-    {
-        IEnumerable<Client> clients = await _appDbContext.Clients.ToListAsync(cancellationToken);
-        return clients;
-    }
-
-    [HttpGet("id/{id}")]
-    public async Task<Client> GetClientByIdAsync([FromRoute] Guid id, CancellationToken cancellationToken)
-    {
-        Client client = await _appDbContext.Clients.FirstOrDefaultAsync(t => t.Id == id, cancellationToken: cancellationToken) ?? throw new NotFoundEntityException("Not found siski");
-        return client;
-    }
-
-    [HttpGet("phone/{phoneNumber}")]
-    public async Task<Client> GetClientByPhoneAsync([FromRoute] string phoneNumber, CancellationToken cancellationToken)
-    {
-        if (phoneNumber.Contains('+'))
+        public ClientContoller(AppDbContext appDbContext)
         {
-            Client client = await _appDbContext.Clients.FirstOrDefaultAsync(t => t.PhoneNumber == phoneNumber, cancellationToken: cancellationToken) ?? throw new NotFoundEntityException("Not found siski");
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<Client> CreateClientAsync([FromBody] Client client, CancellationToken cancellationToken)
+        {
+            await _appDbContext.AddAsync(client, cancellationToken);
+            await _appDbContext.SaveChangesAsync(cancellationToken);
             return client;
         }
-        return null;
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> GetClientsAsync(CancellationToken cancellationToken)
+        {
+            IEnumerable<Client> clients = await _appDbContext.Clients.ToListAsync(cancellationToken);
+            return clients;
+        }
+
+
+        [HttpGet("phone/{phoneNumber}")]
+        public async Task<Client> GetClientByPhoneAsync([FromRoute] string phoneNumber, CancellationToken cancellationToken)
+        {
+            if (phoneNumber.Contains('+'))
+            {
+                Client client = await _appDbContext.Clients.FirstOrDefaultAsync(t => t.PhoneNumber == phoneNumber, cancellationToken: cancellationToken) ?? throw new NotFoundEntityException("Not found siski");
+                return client;
+            }
+            return null;
+        }
+
+        [HttpPost("changePhone/{phoneNumber}/{newPhoneNumber}")]
+        public async Task<bool> ChangePhoneNumberAsync([FromRoute] string phoneNumber, [FromRoute] string newPhoneNumber, CancellationToken cancellationToken)
+        {
+            if (!Regex.IsMatch(phoneNumber, @"^\d{9}$|^\d{12}$"))
+            {
+                return false;
+            }
+
+            Client? client = await _appDbContext.Clients.FirstOrDefaultAsync(t => t.PhoneNumber == phoneNumber, cancellationToken);
+
+            if (client is not null)
+            {
+                client.PhoneNumber = newPhoneNumber;
+                await _appDbContext.SaveChangesAsync(cancellationToken);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-
-    // public async Task<Client> ChangePhoneNumberAsync([FromRoute] string phoneNumber)
-
 }
--- BankApp.API/User.cs
+++ BankApp.API/User.cs
@@ -1,90 +1,66 @@
-namespace DataAccess;
-// using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
-public class Client
+namespace DataAccess
 {
-    private string _phoneNumber;
-    private DateTime _createdAt;
+    public class Client
+    {
+        private DateTime _createdAt;
 
-    public Guid Id { get; set; }
-    public DateTime CreatedAt
-    {
-        get => _createdAt;
-        private set => _createdAt = value;
-    }
-    public required string FirstName { get; set; }
-    public string? MiddleName { get; set; }
-    public string? LastName { get; set; }
-    // public List<Deposit>? Deposits { get; set; }
-
-    [PhoneNumber(7)]
-    [Required(ErrorMessage = "Phone number is required")]
-    public required string PhoneNumber
-    {
-        get => _phoneNumber;
-        set => _phoneNumber = InternationalFormat(value);
-    }
-
-    public Client()
-    {
-        _createdAt = DateTime.UtcNow.Date;
-    }
-    private static string InternationalFormat(string phoneNumber)
-    {
-        if (!phoneNumber.StartsWith("+"))
+        public Guid Id { get; set; }
+        public DateTime CreatedAt
         {
-            return $"+{phoneNumber}";
+            get => _createdAt;
+            private set => _createdAt = value;
         }
-        return phoneNumber;
-    }
-    
-}
+        public required string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
 
+        [PhoneNumber(7)]
+        [Required(ErrorMessage = "Phone number is required")]
+        [StringLength(12, MinimumLength =9)]
+        public required string PhoneNumber { get; set; }
 
-public class Deposit
-{
-    public Guid Id { get; set; }
-    public required string Type { get; set; }
-    public decimal Balance { get; set; }
-    public ushort Percentage { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public required Client Owner { get; set; }
-}
-
-
-
-public class PhoneNumberAttribute : ValidationAttribute
-{
-    private readonly int _requiredLength;
-
-    public PhoneNumberAttribute(int requiredLength)
-    {
-        _requiredLength = requiredLength;
-    }
-
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-    {
-        var phoneNumber = value as string;
-
-        if (string.IsNullOrEmpty(phoneNumber))
+        public Client()
         {
-            return new ValidationResult("Phone number is required.");
+            _createdAt = DateTime.UtcNow.Date;
         }
 
-        if (!phoneNumber.StartsWith("+"))
+
+    }
+
+
+    public class Deposit
+    {
+        public Guid Id { get; set; }
+        public required string Type { get; set; }
+        public decimal Balance { get; set; }
+        public ushort Percentage { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public required Client Owner { get; set; }
+    }
+
+
+
+    public class PhoneNumberAttribute : ValidationAttribute
+    {
+        private readonly int _requiredLength;
+
+        public PhoneNumberAttribute(int requiredLength)
         {
-            return new ValidationResult("Phone number must start with '+'.");
+            _requiredLength = requiredLength;
         }
 
-        // Remove the "+" and count the digits
-        var digitsOnly = Regex.Replace(phoneNumber, @"\D", "");
-        if (digitsOnly.Length != _requiredLength)
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return new ValidationResult($"Phone number must contain exactly {_requiredLength} digits.");
-        }
+            var phoneNumber = value as string;
 
-        return ValidationResult.Success;
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return new ValidationResult("Phone number is required.");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
